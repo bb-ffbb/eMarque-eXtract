1009bd5db8d2c6620ecd1bc735f1609c3c022cd1
Alexandre Duros <alexandre.duros@gmail.com>
2017-10-31 00:51:53 +0100

    Wip: parsing em files

---
+++ GameFileConverter/Converter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+using static Newtonsoft.Json.JsonConvert;
+
+
+namespace GameFileConverter
+{
+    public class Binder : System.Runtime.Serialization.SerializationBinder
+    {
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            if (assemblyName ==
+                "Basket2, Version=1.0.0.4, Culture=neutral, PublicKeyToken=null")
+            {
+                string className = "GameFileConverter.Game";
+
+                assemblyName = Assembly.GetExecutingAssembly().FullName;
+
+                switch (typeName)
+                {
+                    case "Basket2.Model.Equipe":
+                        className = "GameFileConverter.Team";
+                        break;
+                    case "Basket2.Model.Rencontre":
+                        className = "GameFileConverter.Game";
+                        break;
+                    default:
+                        Console.WriteLine(String.Format("cannot find type to bind to ${0}", typeName));
+                        break;
+                }
+
+                return Type.GetType(String.Format("{0}, {1}", className, assemblyName));
+            }
+
+            return Type.GetType("string");
+        }
+    }
+
+    public class Converter
+    {
+        static void Main(string[] args)
+        {
+            foreach (string file in args) 
+            {
+                FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Binder = new GameFileConverter.Binder();
+                object game = binaryFormatter.Deserialize((Stream) fileStream);
+                string json = SerializeObject(game);
+                Console.WriteLine(json);                
+            }
+        }
+    }
+
+    [Serializable]
+    public class Game 
+    {
+        [JsonProperty(PropertyName = "totalPlayedTime")]
+        public TimeSpan _tpsJeuTotal;
+
+        [JsonProperty(PropertyName = "localTeam")]
+        public Team EquipeA{ get; private set; }
+
+        [JsonProperty(PropertyName = "visitorTeam")]
+        public Team EquipeB{ get; private set; }
+    }
+
+    [Serializable]
+    public class Team 
+    {
+        [JsonProperty(PropertyName = "fullName")]
+        public string _nom;
+
+        [JsonProperty(PropertyName = "shortName")]
+        public string _abrege;
+
+        [JsonProperty(PropertyName = "digitalNumber")]
+        public string _numInformatique;
+    }
+}
